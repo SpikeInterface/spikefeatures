06a02245780e4d4b06c9f637c804f79481e4846d
Alessio Buccino <alejoe9187@gmail.com>
2020-04-23 09:49:37 +0200

    Added dummy tests (for travis)

---
+++ spikefeatures/tests/test_features.py
@@ -0,0 +1,19 @@
+from spikefeatures import *
+
+def test_calculate_features():
+    pass
+
+def test_peak_to_valley():
+    pass
+
+def test_halfwidth():
+    pass
+
+def test_peak_trough_ratio():
+    pass
+
+def test_repolarization_slope():
+    pass
+
+def test_recovery_slope():
+    pass
